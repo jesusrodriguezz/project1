c750e706d386cfd1784bc811380e13c2a6bed8c5
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-06-22 14:55:32 -0700

    Added the DataFrame and engine

--- project1.py
+++ project1.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import sqlalchemy as db
 import requests
 import json
 
@@ -7,3 +9,14 @@ API_KEY = "AIzaSyAYP2C_aLg1ll-oYvwwdgaYjAJJVGNNMDM "
 url = ' https://www.googleapis.com/youtube/v3'
 response = requests.post(url)
 print(response)
+
+df = pd.DataFrame.from_dict()
+print(df)
+
+engine = db.create_engine('')
+
+df.to_sql('', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
