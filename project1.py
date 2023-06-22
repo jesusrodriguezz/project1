cb18580e7183858e0073265522ae193c3de632b1
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-06-22 13:26:25 -0700

    added API_KEY

--- project1.py
+++ project1.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+
+API_KEY = "AIzaSyAYP2C_aLg1ll-oYvwwdgaYjAJJVGNNMDM "
+
 url = ' https://www.googleapis.com/youtube/v3'
 response = requests.post(url)
 print(response)
