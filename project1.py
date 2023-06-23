e05b23876200168c7ad165f9fb2d57cf3f6ec28a
jesusrodriguezz <130525663+jesusrodriguezz@users.noreply.github.com>
2023-06-23 14:54:39 -0700

    Updated url

--- project1.py
+++ project1.py
@@ -6,7 +6,7 @@ import json
 
 API_KEY = "AIzaSyAYP2C_aLg1ll-oYvwwdgaYjAJJVGNNMDM "
 
-url = ' https://www.googleapis.com/youtube/v3'
+url = 'https://developers.google.com/youtube/v3/'
 response = requests.post(url)
 print(response)
 
